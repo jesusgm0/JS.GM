81d24b7a8b55e3811e44201edf4b1ea9ec856447
jesusgm0 <gabrielmarioj1@gmail.com>
2022-09-22 14:14:48 -0300

    Ex15.09

---
+++ funcao.js
@@ -0,0 +1,85 @@
+// //let n1, n2, resultado
+// //n1= prompt("Digite o número:")
+// //n2= prompt("Digite o número:")
+// function abacaxi(n1, n2){
+//     resultado= n1+ n2
+//     return alert(resultado)
+
+//}
+
+//questao1
+// let num1, num2, resultado, pct
+// num1 = prompt("Digite o número")
+// num2= prompt("Digite a porcentagem desejada")
+// function Porcentagem(num1, num2){
+//     resultado = (num1*num2)/100
+//     return alert(num2+"% de " +num1+" é igual a " + resultado)
+// }
+// Porcentagem(num1, num2)
+
+//questao2
+
+// let grauF, tempC;
+// tempC= prompt("Digite a temperatura, em Celsius: ")
+
+// function Temp(tempC, grauF){
+// grauF = parseInt((tempC*9)/5) +parseInt(32)
+// return alert("A temperatura, em Fahrenheit, é: "+ grauF+"ºF")
+// }
+
+// Temp(tempC,grauF)
+
+//questao3
+// let grauC, tempF;
+// tempF= prompt("Digite a temperatura, em Fahrenheit: ")
+
+// function Temp(tempF, grauC){
+// grauC = parseInt((tempF-32)*5)/9 
+// return alert("A temperatura, em Celsius, é: "+ grauC.toFixed(1)+ "ºC")
+// }
+
+// Temp(tempF,grauC)
+//
+//questao4
+
+// let peso, altura, res
+// peso = prompt("Digite seu peso, em Kg:(Favor não usar vírgula, apenas ponto)")
+// altura= prompt("Digite sua altura, em metros(Favor não usar vírgula, apenas ponto):")
+
+// function Imc(peso, altura){
+// res=(peso)/parseFloat(altura*altura)
+// return alert("O valor de seu IMC é: "+ res.toFixed(1) +".")
+// }
+// Imc(peso, altura)
+
+// //questao5
+// let num1, num2, num3, res1, res2
+// num1 =prompt("Digite o primeiro número:")
+// num2 =prompt("Digite o segundo número:")
+
+// num3 =prompt("Digite o terceiro número:")
+
+// function compMai(){
+// res1= Math.max(num1,num2,num3)
+//     return alert(" O número de maior valor é: "+ res1)
+// }
+//   compMai() 
+//   function compMen(){
+//   res2=Math.min(num1, num2, num3)
+//   return alert("E o número de menor valor é: "+ res2)
+//   }
+//   compMen()
+
+//questao6
+// let custo, taxaImp, res1, res2
+// custo= prompt("Digite o valor do item:")
+// taxaImp= prompt("Digite a taxa do Imposto:")
+
+// function somaImposto(custo, taxaImp){
+// res1 = parseFloat(taxaImp/100)*custo
+// res2= parseFloat(res1)+parseFloat(custo)
+// return alert("O valor do  imposto é "+ res1+ ", e o valor total, junto com o imposto é "+ res2)
+// }
+// somaImposto(custo, taxaImp, res1, res2)
+
+
